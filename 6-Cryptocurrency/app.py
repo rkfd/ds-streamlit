9cc1a1781e45e89fa2184ed496c5384a6c472624
Satwik Gawand <satwikgawand@gmail.com>
2021-01-17 18:43:35 +0530

    scrape cryptocurrency data from coinmarketcap

--- 6-Cryptocurrency/app.py
+++ 6-Cryptocurrency/app.py
@@ -34,3 +34,51 @@ col1.header('Input Options')
 
 # Sidebar - Currency Price Unit
 currency_price_unit = col1.selectbox('Select Currency for Price', ('USD', 'BTC', 'ETH'))
+
+# Webscraping CoinMarketCap Data
+@st.cache
+def load_data():
+    # Fetch Data from URL
+    cmc = requests.get('https://coinmarketcap.com')
+    soup = BeautifulSoup(cmc.content, 'html.parser')
+
+    # Select Data from the Fetch
+    data = soup.find('script', id='__NEXT_DATA__', type='application/json')
+    coins = {}
+    coin_data = json.loads(data.contents[0])
+    listings = coin_data['props']['initialState']['cryptocurrency']['listingLatest']['data']
+    for i in listings:
+        coins[str(i['id'])] = i['slug']
+
+    coin_name = []
+    coin_symbol = []
+    market_cap = []
+    percent_change_1h = []
+    percent_change_24h = []
+    percent_change_7d = []
+    price = []
+    volume_24h = []
+
+    for i in listings:
+        coin_name.append(i['slug'])
+        coin_symbol.append(i['symbol'])
+        price.append(i['quote'][currency_price_unit]['price'])
+        percent_change_1h.append(i['quote'][currency_price_unit]['percent_change_1h'])
+        percent_change_24h.append(i['quote'][currency_price_unit]['percent_change_24h'])
+        percent_change_7d.append(i['quote'][currency_price_unit]['percent_change_7d'])
+        market_cap.append(i['quote'][currency_price_unit]['market_cap'])
+        volume_24h.append(i['quote'][currency_price_unit]['volume_24h'])
+
+    df = pd.DataFrame(columns=['coin_name','coin_symbol','market_cap', 'percent_change_1h', 'percent_change_24h', 'percent_change_7d', 'price', 'volume_24h'])
+    df['coin_name'] = coin_name
+    df['coin_symbol'] = coin_symbol
+    df['market_cap'] = market_cap
+    df['percent_change_1h'] = percent_change_1h
+    df['percent_change_24h'] = percent_change_24h
+    df['percent_change_7d'] = percent_change_7d
+    df['price'] = price
+    df['volume_24h'] = volume_24h
+
+    return df
+
+df = load_data()
