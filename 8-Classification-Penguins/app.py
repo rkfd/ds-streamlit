3b6ded753c1180c541cb9c461b0c43dd451d0c22
Satwik Gawand <satwikgawand@gmail.com>
2021-01-18 02:27:42 +0530

    added header, intro

--- 8-Classification-Penguins/app.py
+++ 8-Classification-Penguins/app.py
@@ -1,6 +1,22 @@
+'''
+    File name: app.py
+    Author: Satwk Gawand
+    Date Created: 18/1/2021
+    Date Last Modified: 18/1/2021
+    Python Version: 3.8.6
+'''
+
+# Imports
 import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
 
 from sklearn.ensemble import RandomForestClassifier
+
+# Intro
+st.write("""
+# Penguin Prediction App
+This app predicts the **Palmer Penguin** species!
+Data obtained form the [PalmerPenguins library](https://github.com/allisonhorst/palmerpenguins) in R by Allison Horst.
+""")
