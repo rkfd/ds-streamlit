4fd5c5eb2823890d7c4d906263bcf8c43d691164
Satwik Gawand <satwikgawand@gmail.com>
2021-01-17 12:39:07 +0530

    data scraping and preprocessing

--- 4-EDA-Football/app.py
+++ 4-EDA-Football/app.py
@@ -12,3 +12,19 @@ This app performs simple webscraping of NFL Footbal player stats data (focusing 
 * **Python Libraries:** base64, pandas, streamlit, numpy, matplotlib, seaborn
 * **Data Source:** [pro-football-reference.com](https://www.pro-football-reference.com/).
 """)
+
+st.sidebar.header('User Input Features')
+selected_year = st.sidebar.selectbox('Year', list(reversed(range(1990,2020))))
+
+@st.cache
+def load_data(year):
+    url = "https://www.pro-football-reference.com/years/" + str(year) + "/rushing.htm"
+    html = pd.read_html(url, header=1)
+    df = html[0]
+    raw = df.drop(df[df.Age == 'Age'].index)
+    raw = raw.fillna(0)
+    playerstats = raw.drop(['Rk'], axis=1)
+    return playerstats
+
+playerstats = load_data(selected_year)
+playerstats
