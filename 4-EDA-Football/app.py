05cf10f5a9afc95f928c108268783b3f12f52cdd
Satwik Gawand <satwikgawand@gmail.com>
2021-01-17 13:05:31 +0530

    download done

--- 4-EDA-Football/app.py
+++ 4-EDA-Football/app.py
@@ -54,3 +54,5 @@ def filedownload(df):
     b64 = base64.b64encode(csv.encode()).decode()
     href = f'<a href="data:file/csv;base64,{b64}" download="playerstats.csv">Download CSV File</a>'
     return href
+
+st.markdown(filedownload(df_selected_team), unsafe_allow_html=True)
