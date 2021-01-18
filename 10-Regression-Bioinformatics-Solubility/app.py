751f2236b4edf9f909c0c2ca63bb0815a10d7420
Satwik Gawand <satwikgawand@gmail.com>
2021-01-18 09:27:01 +0530

    project init

---
+++ 10-Regression-Bioinformatics-Solubility/app.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+    File name: app.py
+    Author: Satwik Gawand
+    Date Created: 18/1/2021
+    Date Last Modified: 18/1/2021
+    Python Version: 3.8.6
+'''
