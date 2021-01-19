76c15fd7ac6bc30c8d16809d73199e4dd631c827
Satwik Gawand <satwikgawand@gmail.com>
2021-01-19 16:15:48 +0530

    Course Completed

--- 10-Regression-Bioinformatics-Solubility/app.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-'''
-    File name: app.py
-    Author: Satwik Gawand
-    Date Created: 18/1/2021
-    Date Last Modified: 18/1/2021
-    Python Version: 3.8.6
-'''
